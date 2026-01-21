161510cd7002e6520518c0b3f6a937d887c8c95d
AndresCortes31 <acortes@loto.hn>
2026-01-21 15:20:04 -0600

    cambio pantalla 3

--- paginas/num_sorteados_semana.php
+++ paginas/num_sorteados_semana.php
@@ -13,50 +13,53 @@
         <div class="cal-izq">
 
             <!-- SELECTORES -->
-            <div class="selectores">
+            <div class="filtros-semana">
 
-                <div class="selector-anio">
-                    <span class="label-anio">SELECCIONÁ AÑO</span>
-                    <div class="anio-dropdown">
-                        <span class="anio-activo"><?= $anioActual ?></span>
-                        <span class="flecha">&#9662;</span>
-                    </div>
-
-                    <div class="anio-opciones">
-                        <?php foreach ($aniosDisponibles as $anio): ?>
-                            <div data-anio="<?= $anio ?>"><?= $anio ?></div>
-                        <?php endforeach; ?>
-                    </div>
-                </div>
-
-                <div class="selector-mes">
-                    <span class="label-anio">SELECCIONÁ MES</span>
-                    <div class="mes-dropdown">
-                        <span class="mes-activo">Todos los meses</span>
-                        <span class="flecha">&#9662;</span>
-                    </div>
-
-                    <div class="mes-opciones">
-                        <!-- JS las llena -->
+                <!-- AÑO -->
+                <div class="filtro-anio">
+                    <span class="filtro-label">SELECCIONÁ AÑO</span>
+                    <div class="selector-anio">
+                        <div class="anio-dropdown">
+                            <span class="anio-activo"><?= $anioActual ?></span>
+                            <span class="flecha">&#9662;</span>
+                        </div>
+                        <div class="anio-opciones">
+                            <?php foreach ($aniosDisponibles as $anio): ?>
+                                <div data-anio="<?= $anio ?>"><?= $anio ?></div>
+                            <?php endforeach; ?>
+                        </div>
                     </div>
                 </div>
 
+                <!-- MES + DÍA -->
+                <div class="filtros-secundarios">
 
-                <div class="selector-dia">
-                    <span class="label-anio">SELECCIONÁ DÍA</span>
-
-                    <div class="dia-dropdown">
-                        <span class="dia-activo">Lunes</span>
-                        <span class="flecha">&#9662;</span>
+                    <div class="filtro">
+                        <span class="filtro-label">SELECCIONÁ MES</span>
+                        <div class="selector-mes">
+                            <div class="mes-dropdown">
+                                <span class="mes-activo">Todos los meses</span>
+                                <span class="flecha">&#9662;</span>
+                            </div>
+                            <div class="mes-opciones"></div>
+                        </div>
                     </div>
 
-                    <div class="dia-opciones">
-                        <!-- JS las llena -->
+                    <div class="filtro">
+                        <span class="filtro-label">SELECCIONÁ DÍA</span>
+                        <div class="selector-dia">
+                            <div class="dia-dropdown">
+                                <span class="dia-activo">Lunes</span>
+                                <span class="flecha">&#9662;</span>
+                            </div>
+                            <div class="dia-opciones"></div>
+                        </div>
                     </div>
+
                 </div>
-
 
             </div>
+
 
 
             <!-- LISTADO -->
